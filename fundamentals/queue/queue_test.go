f08ac76ee729644388062e73d1585e85b361424a
jefersonf <jefersonnpn@gmail.com>
2022-12-09 18:57:16 -0300

    updates testing error messages

--- fundamentals/queue/queue_test.go
+++ fundamentals/queue/queue_test.go
@@ -6,16 +6,16 @@ func TestFront(t *testing.T) {
 	q1 := New(1, 2)
 	v, err := q1.Front()
 	if err != nil {
-		t.Error("Non-empty queue, must return nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if v != 1 {
-		t.Errorf("Top() = %v, want %v", v, 1)
+		t.Errorf("got %v, want %v", v, 1)
 	}
 	a := make([]int, 0)
 	q2 := New(a...)
 	_, err = q2.Front()
 	if err == nil {
-		t.Errorf("Empty queue, want %v", err)
+		t.Errorf("got nil, want %v", errIndexOutOfBound)
 	}
 }
 
@@ -24,19 +24,19 @@ func TestPopBack(t *testing.T) {
 	sz := q1.size
 	v, err := q1.PopBack()
 	if err != nil {
-		t.Error("Non-empty queue, must return nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if v != 2 {
-		t.Errorf("PopBack() = %v, want %v", v, 2)
+		t.Errorf("got %v, want %v", v, 2)
 	}
 	if q1.size != int64(sz-1) {
-		t.Errorf("Queue size is still %v, want %v", sz, sz-1)
+		t.Errorf("got %v, want %v", sz, sz-1)
 	}
 	a := make([]int, 0)
 	q2 := New(a...)
 	_, err = q2.PopBack()
 	if err == nil {
-		t.Errorf("Empty queue, want %v", err)
+		t.Errorf("got nil, want %v", errIndexOutOfBound)
 	}
 }
 
@@ -45,19 +45,19 @@ func TestPopFront(t *testing.T) {
 	sz := q1.size
 	v, err := q1.PopFront()
 	if err != nil {
-		t.Error("Non-empty queue, want nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if v != 1 {
-		t.Errorf("PopFront() = %v, want %v", v, 1)
+		t.Errorf("got %v, want %v", v, 1)
 	}
 	if q1.size != int64(sz-1) {
-		t.Errorf("Queue size is still %v, want %v", sz, sz-1)
+		t.Errorf("got %v, want %v", sz, sz-1)
 	}
 	a := make([]int, 0)
 	q2 := New(a...)
 	_, err = q2.PopFront()
 	if err == nil {
-		t.Errorf("Empty queue, want %v", err)
+		t.Errorf("got nil, want %v", errIndexOutOfBound)
 	}
 }
 
@@ -69,13 +69,13 @@ func TestPush(t *testing.T) {
 	q1.Push(v)
 	front, err := q1.Front()
 	if err != nil {
-		t.Error("Non-empty queue, want nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if front != v {
-		t.Errorf("Get %v, want %v", front, v)
+		t.Errorf("got %v, want %v", front, v)
 	}
 	if q1.size != int64(sz+1) {
-		t.Errorf("Queue size is still %v, want %v", q1.size, sz-1)
+		t.Errorf("got %v, want %v", q1.size, sz-1)
 	}
 }
 
@@ -83,6 +83,6 @@ func TestSize(t *testing.T) {
 	q1 := New(1, 2, 3)
 	sz := q1.size
 	if q1.Size() != sz {
-		t.Errorf("queue size must be %v", sz)
+		t.Errorf("got %v, want %v", q1.Size(), sz)
 	}
 }
--- fundamentals/stack/stack_test.go
+++ fundamentals/stack/stack_test.go
@@ -8,16 +8,16 @@ func TestTop(t *testing.T) {
 	s1 := New(1, 2)
 	v, err := s1.Top()
 	if err != nil {
-		t.Error("Non-empty stack, must return nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if v != 2 {
-		t.Errorf("Top() = %v, want %v", v, 2)
+		t.Errorf("got %v, want %v", v, 2)
 	}
 	a := make([]int, 0)
 	s2 := New(a...)
 	_, err = s2.Top()
 	if err == nil {
-		t.Error("Empty stack")
+		t.Errorf("got nil, want %v", errIndexOutOfBound)
 	}
 }
 
@@ -26,22 +26,22 @@ func TestPop(t *testing.T) {
 	sz := s1.size
 	v1, err := s1.Pop()
 	if err != nil {
-		t.Error("Non-empty stack, must return nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if v1 != 3 {
-		t.Errorf("Pop() = %v, want %v", v1, 3)
+		t.Errorf("got %v, want %v", v1, 3)
 	}
 	if s1.size != int64(sz-1) {
-		t.Errorf("Stack size is still %v, want %v", sz, sz-1)
+		t.Errorf("got %v, want %v", sz, sz-1)
 	}
 	s1.Pop()
 	v2, _ := s1.Pop()
 	if v2 != 1 {
-		t.Errorf("Pop() = %v, want %v", v2, 1)
+		t.Errorf("got %v, want %v", v2, 1)
 	}
 	_, err = s1.Pop()
 	if err == nil {
-		t.Errorf("Empty stack, must return an error: %v", err)
+		t.Errorf("got nil, want %v", errIndexOutOfBound)
 	}
 }
 
@@ -53,13 +53,13 @@ func TestPush(t *testing.T) {
 	s1.Push(v)
 	top, err := s1.Top()
 	if err != nil {
-		t.Error("Non-empty stack, must result nil")
+		t.Errorf("got %v, want nil", err)
 	}
 	if top != v {
-		t.Errorf("Get %v, want %v", top, v)
+		t.Errorf("got %v, want %v", top, v)
 	}
 	if s1.size != int64(sz+1) {
-		t.Errorf("Stack size is still %v, want %v", s1.size, sz-1)
+		t.Errorf("got %v, want %v", s1.size, sz-1)
 	}
 }
 
@@ -67,6 +67,6 @@ func TestSize(t *testing.T) {
 	s1 := New(1, 2, 3)
 	sz := s1.size
 	if s1.Size() != sz {
-		t.Errorf("stack size must be %v", sz)
+		t.Errorf("got %v, want %v", s1.Size(), sz)
 	}
 }
