43065a2c0312eb545ea9ba72c7607e3b2b52c67b
Jeferson <jefersonnpn@gmail.com>
2023-08-22 19:39:28 -0300

    :art: run gofmt

--- tree/aho-corasick/aho-corasick.go
+++ tree/aho-corasick/aho-corasick.go
@@ -1,27 +1,27 @@
 package aho_corasick
 
 type Vertex struct {
-   Next []int
-   Output bool
+	Next   []int
+	Output bool
 }
 
 type Trie struct {
-   Vertices []Vertex
+	Vertices []Vertex
 }
 
 func (v *Vertex) newVertex() *Vertex {
-   s := make([]int, 26)
-   for i := range s {
-     s[i] = -1
-  } 
-  return &Vertex{
-   Next: s,
-   Output: false,
-}
+	s := make([]int, 26)
+	for i := range s {
+		s[i] = -1
+	}
+	return &Vertex{
+		Next:   s,
+		Output: false,
+	}
 }
 
 func New(size int) *Trie {
-   return &Trie{
-    Vertices: make([]Vertex, 1),
-}
+	return &Trie{
+		Vertices: make([]Vertex, 1),
+	}
 }
