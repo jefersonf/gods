d20827d438a7f931e8dcff77fddc19686e2bcd4c
Jeferson <jefersonnpn@gmail.com>
2023-08-30 18:37:01 -0300

    :white_check_mark: add trie height test

---
+++ tree/aho-corasick/trie_test.go
@@ -0,0 +1,48 @@
+package ahocorasick
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTrieHeight(t *testing.T) {
+
+	testcases := []struct {
+		stringSet  []string
+		trieHeight int
+	}{
+		{
+			stringSet:  []string{},
+			trieHeight: 0,
+		},
+		{
+			stringSet:  []string{"foo"},
+			trieHeight: 3,
+		},
+		{
+			stringSet:  []string{"pot", "bowl"},
+			trieHeight: 4,
+		},
+		{
+			stringSet:  []string{"paper", "crayons", "notebook"},
+			trieHeight: 8,
+		},
+		{
+			stringSet:  []string{"chair", "desk", "calculator", "pencil"},
+			trieHeight: 10,
+		},
+	}
+
+	for i, tc := range testcases {
+		t.Run(fmt.Sprintf("StringSet_%d", i), func(t *testing.T) {
+			trie := New()
+			for _, s := range tc.stringSet {
+				trie.AddString(s)
+			}
+			trieHeight := trie.Height()
+			if trieHeight != tc.trieHeight {
+				t.Errorf("Incorrect trie size; got %d, want %d", trieHeight, tc.trieHeight)
+			}
+		})
+	}
+}
