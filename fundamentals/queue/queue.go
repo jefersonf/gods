904b6803a4f796c2a650ef3e1c0df450bdba1124
Jeferson <jefersonnpn@gmail.com>
2023-09-04 13:25:29 -0300

    :recycle:  makes queue accept fully generic types

--- fundamentals/queue/queue.go
+++ fundamentals/queue/queue.go
@@ -2,37 +2,32 @@ package queue
 
 import (
 	"errors"
-
-	"golang.org/x/exp/constraints"
 )
-
-// Number represents a numeric value.
-type Number interface {
-	constraints.Float | constraints.Integer
-}
 
 var (
 	// errIndexOutOfBound is an error that accurs whenever an invalid invalid index is accessed.
 	errIndexOutOfBound = errors.New("index out of bound")
 )
 
-type Queue[T Number] struct {
-	size int64
+type Queue[T any] struct {
+	size uint64
 	data []T
 }
 
-func New[T Number](nums ...T) *Queue[T] {
+func New[T any](nums ...T) *Queue[T] {
 	return &Queue[T]{
-		size: int64(len(nums)),
+		size: uint64(len(nums)),
 		data: nums,
 	}
 }
 
-func (q *Queue[T]) Front() (T, error) {
+func (q *Queue[T]) Front() (v T, err error) {
 	if q.size < 1 {
-		return 0, errIndexOutOfBound
+		err = errIndexOutOfBound
+		return v, err
 	}
-	return q.data[0], nil
+	v = q.data[0]
+	return v, nil
 }
 
 func (q *Queue[T]) Push(x T) {
@@ -40,31 +35,33 @@ func (q *Queue[T]) Push(x T) {
 	q.size += 1
 }
 
-func (q *Queue[T]) PopBack() (T, error) {
+func (q *Queue[T]) PopBack() (v T, err error) {
 	if q.size < 1 {
-		return 0, errIndexOutOfBound
+		err = errIndexOutOfBound
+		return v, err
 	}
 	t := make([]T, q.size-1)
 	b := q.data[q.size-1]
 	copy(t, q.data[:q.size-1])
 	q.size -= 1
 	q.data = t
-	return b, nil
+	return b, err
 }
 
-func (q *Queue[T]) PopFront() (T, error) {
+func (q *Queue[T]) PopFront() (v T, err error) {
 	if q.size < 1 {
-		return 0, errIndexOutOfBound
+		err = errIndexOutOfBound
+		return v, err
 	}
 	t := make([]T, q.size-1)
 	f := q.data[0]
 	copy(t, q.data[1:])
 	q.size -= 1
 	q.data = t
-	return f, nil
+	return f, err
 }
 
-func (q Queue[T]) Size() int64 {
+func (q Queue[T]) Size() uint64 {
 	return q.size
 }
 
