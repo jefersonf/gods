7a544b9c8e2669b02d251d90bf52568ebe4d90a5
Jeferson <jefersonnpn@gmail.com>
2023-09-12 17:37:52 -0300

    :test_tube:  add TestFindSet test

--- tree/dsu/dsu_test.go
+++ tree/dsu/dsu_test.go
@@ -73,3 +73,70 @@ func TestSize(t *testing.T) {
 	}
 
 }
+
+func TestFindSet(t *testing.T) {
+
+	testcases := []struct {
+		n        uint64
+		id       uint64
+		parentId uint64
+		unions   [][2]uint64
+	}{
+		{
+			n:        1,
+			id:       1,
+			parentId: 1,
+			unions:   make([][2]uint64, 0),
+		},
+		{
+			n:        3,
+			id:       2,
+			parentId: 1,
+			unions:   [][2]uint64{{1, 2}},
+		},
+		{
+			n:        3,
+			id:       3,
+			parentId: 3,
+			unions:   [][2]uint64{{1, 2}},
+		},
+		{
+			n:        3,
+			id:       3,
+			parentId: 1,
+			unions:   [][2]uint64{{1, 2}, {2, 3}},
+		},
+		{
+			n:        4,
+			id:       4,
+			parentId: 3,
+			unions:   [][2]uint64{{1, 2}, {3, 4}},
+		},
+		{
+			n:        4,
+			id:       4,
+			parentId: 3,
+			unions:   [][2]uint64{{2, 1}, {4, 3}},
+		},
+		{
+			n:        5,
+			id:       3,
+			parentId: 1,
+			unions:   [][2]uint64{{5, 1}, {4, 3}, {2, 1}, {4, 5}},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("ArrayOfLength=%d-Unions=%d", tc.n, len(tc.unions)), func(t *testing.T) {
+			dsu := New(tc.n)
+			for _, p := range tc.unions {
+				dsu.UnionSets(p[0], p[1])
+			}
+			parentId := dsu.FindSet(tc.id)
+			if parentId != tc.parentId {
+				t.Errorf("Got %v, want %v", parentId, tc.parentId)
+			}
+		})
+	}
+
+}
