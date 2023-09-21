b7e013b96e5a5632517a70716e9d21d9f97bd7b4
Jeferson <jefersonnpn@gmail.com>
2023-09-20 21:30:25 -0300

    :see_no_evil:  add TestIterativePow

--- algebra/fundamentals/binexp_test.go
+++ algebra/fundamentals/binexp_test.go
@@ -53,3 +53,55 @@ func TestRecursivePow(t *testing.T) {
 		}
 	}
 }
+
+func TestIterativePow(t *testing.T) {
+
+	testcases := []struct {
+		base uint64
+		exp  uint64
+		pow  uint64
+	}{
+		{
+			base: 1,
+			exp:  0,
+			pow:  1,
+		},
+		{
+			base: 1,
+			exp:  1,
+			pow:  1,
+		},
+		{
+			base: 1,
+			exp:  3,
+			pow:  1,
+		},
+		{
+			base: 2,
+			exp:  5,
+			pow:  32,
+		},
+		{
+			base: 2,
+			exp:  8,
+			pow:  256,
+		},
+		{
+			base: 3,
+			exp:  4,
+			pow:  81,
+		},
+		{
+			base: 3,
+			exp:  7,
+			pow:  2187,
+		},
+	}
+
+	for _, tc := range testcases {
+		pow := IterativePow(tc.base, tc.exp)
+		if pow != tc.pow {
+			t.Errorf("Pow(%d, %d), Got %d, want %d", tc.base, tc.exp, pow, tc.pow)
+		}
+	}
+}
