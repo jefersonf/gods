4228c9bb42958c5ab1bba547dea1da01c8847277
Jeferson <jefersonnpn@gmail.com>
2023-09-22 18:35:13 -0300

    :test_tube:   add lcm tests

--- algebra/fundamentals/gcd_test.go
+++ algebra/fundamentals/gcd_test.go
@@ -6,6 +6,7 @@ type GCDTestCase struct {
 	a   int
 	b   int
 	gcd int
+	lcm int
 }
 
 func loadGCDTestCases() []GCDTestCase {
@@ -15,16 +16,25 @@ func loadGCDTestCases() []GCDTestCase {
 			a:   1,
 			b:   1,
 			gcd: 1,
+			lcm: 1,
 		},
 		{
 			a:   2,
 			b:   4,
 			gcd: 2,
+			lcm: 4,
 		},
 		{
 			a:   4,
 			b:   16,
 			gcd: 4,
+			lcm: 16,
+		},
+		{
+			a:   336,
+			b:   3130,
+			gcd: 2,
+			lcm: 525840,
 		},
 	}
 
@@ -45,6 +55,15 @@ func TestIterativeGCD(t *testing.T) {
 		gcd := IterativeGCD(tc.a, tc.b)
 		if tc.gcd != gcd {
 			t.Errorf("IterativeGCD(%d, %d) got %d, want %d", tc.a, tc.b, tc.gcd, gcd)
+		}
+	}
+}
+
+func TestLCM(t *testing.T) {
+	for _, tc := range loadGCDTestCases() {
+		lcm := LCM(tc.a, tc.b)
+		if tc.lcm != lcm {
+			t.Errorf("LCM(%d, %d) got %d, want %d", tc.a, tc.b, tc.lcm, lcm)
 		}
 	}
 }
