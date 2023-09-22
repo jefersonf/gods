8e82f8db3463fe7f18d38e1ae0049d6b4b110f79
Jeferson <jefersonnpn@gmail.com>
2023-09-22 18:16:09 -0300

    :pencil2:   add docs

--- algebra/fundamentals/gcd.go
+++ algebra/fundamentals/gcd.go
@@ -1,5 +1,6 @@
 package fundamentals
 
+// RecursiveGCD returns GCD using recursive approach.
 func RecursiveGCD(a, b int) int {
 	if b == 0 {
 		return a
@@ -7,6 +8,7 @@ func RecursiveGCD(a, b int) int {
 	return RecursiveGCD(b, a%b)
 }
 
+// IterativeGCD returns GCD using iterative approach.
 func IterativeGCD(a, b int) int {
 	for b > 0 {
 		a %= b
@@ -15,6 +17,7 @@ func IterativeGCD(a, b int) int {
 	return a
 }
 
+// GCD returns the greated common divisor of a and b
 func GCD(a, b int, isIterative bool) int {
 	if isIterative {
 		return IterativeGCD(a, b)
