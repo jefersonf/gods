907dc14723519c232f6f3d61fb2b1a9f4bd4bf7c
Jeferson <jefersonnpn@gmail.com>
2023-09-22 19:17:41 -0300

    :test_tube:   fix extended test cases

--- algebra/fundamentals/egcd_test.go
+++ algebra/fundamentals/egcd_test.go
@@ -6,13 +6,15 @@ func TestExtendedGCD(t *testing.T) {
 	var (
 		a         = 55
 		b         = 80
+		x0        = 3
+		y0        = -2
 		gcdResult = 5
 	)
 
 	var x, y int
 	gcd := ExtendedGCD(a, b, &x, &y)
 	if gcd != gcdResult {
-		t.Errorf("Got GCD=%d x=%d y=%d, want GCD=%d x=%d y=%d", gcd, x, y, gcdResult, 0, 0)
+		t.Errorf("Got GCD=%d x=%d y=%d, want GCD=%d x=%d y=%d", gcd, x, y, gcdResult, x0, y0)
 	}
 }
 
@@ -20,12 +22,14 @@ func TestIterativeExtendedGCD(t *testing.T) {
 	var (
 		a         = 55
 		b         = 80
+		x0        = 3
+		y0        = -2
 		gcdResult = 5
 	)
 
 	var x, y int
 	gcd := IterativeExtendedGCD(a, b, &x, &y)
 	if gcd != gcdResult {
-		t.Errorf("Got GCD=%d x=%d y=%d, want GCD=%d x=%d y=%d", gcd, x, y, gcdResult, 0, 0)
+		t.Errorf("Got GCD=%d x=%d y=%d, want GCD=%d x=%d y=%d", gcd, x, y, gcdResult, x0, y0)
 	}
 }
