d12debb2d38b95d3d062e134bbbb2f7b235b538b
Jeferson <jefersonnpn@gmail.com>
2023-09-25 19:45:10 -0300

    :arrow_up:  make sieve integer generic

--- algebra/prime-numbers/sieve.go
+++ algebra/prime-numbers/sieve.go
@@ -1,15 +1,23 @@
 package primenumbers
 
+import "golang.org/x/exp/constraints"
+
+// Number represents a Numeric value.
+type Number interface {
+	constraints.Integer
+}
+
 type sieve []bool
 
-func NewSieve(n int) sieve {
+func NewSieve[T Number](n T) sieve {
 	isPrime := make([]bool, n+1)
 	for i := range isPrime {
 		if i >= 2 {
 			isPrime[i] = true
 		}
 	}
-	for i := 2; i <= n; i++ {
+	var i T
+	for i = 2; i <= n; i++ {
 		if isPrime[i] && i*i <= n {
 			for j := i * i; j <= n; j += i {
 				isPrime[j] = false
